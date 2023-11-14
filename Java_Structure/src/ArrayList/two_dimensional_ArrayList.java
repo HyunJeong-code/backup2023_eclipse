89c3da0a6829c18a0bc08df008ba2893aa1a7da6
김현정 <@192.168.123.104>
2023-11-14 23:25:18 +0900

    2023/11/14 싸피 대비 업로드

---
+++ Java_Structure/src/ArrayList/two_dimensional_ArrayList.java
@@ -0,0 +1,21 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class two_dimensional_ArrayList {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		ArrayList<String>[] arr = new ArrayList[5];
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = new ArrayList<>();
+		}
+		
+		
+
+	}
+
+}
